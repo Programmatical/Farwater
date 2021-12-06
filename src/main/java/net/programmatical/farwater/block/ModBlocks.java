d1b58131075af56b0ddc5694de713808eb490283
Programmatical <aidan@litewire.net>
2021-12-05 20:55:11 -0600

    Update ModBlocks.java

--- src/main/java/net/programmatical/farwater/block/ModBlocks.java
+++ src/main/java/net/programmatical/farwater/block/ModBlocks.java
@@ -2,7 +2,11 @@ package net.programmatical.farwater.block;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SandBlock;
+import net.minecraft.block.GlassBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
@@ -19,7 +23,15 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Farwater.MOD_ID);
 
-    public static final RegistryObject<Block> REFINED_RADIANCE_BLOCK = registerBlock("refined_radiance_block", () -> new Block(AbstractBlock.Properties.of(Material.GLASS).harvestLevel(2).harvestTool(ToolType.PICKAXE).strength(5f).noOcclusion().lightLevel((blockstate) -> 15)));
+    public static final RegistryObject<GlassBlock> REFINED_RADIANCE_BLOCK = registerBlock("refined_radiance_block",
+            () -> new GlassBlock(AbstractBlock.Properties.of(Material.GLASS)
+                    .harvestLevel(2).harvestTool(ToolType.PICKAXE).strength(5f).noOcclusion().lightLevel((blockstate) -> 15).sound(SoundType.GLASS)));
+
+    public static final RegistryObject<SandBlock> ASH_BLOCK = BLOCKS.register("ash_block",
+            () -> new SandBlock(2, AbstractBlock.Properties.of(Material.SAND, MaterialColor.COLOR_BLACK)
+                    .strength(0.3f, 0.3f).harvestTool(ToolType.SHOVEL).harvestLevel(-1)
+                    .sound(SoundType.SAND)
+            ));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
